eba482c277c0611fc2c6e9288cfb6015b122f459
ppcheng <p2cheng0331@gmail.com>
2025-11-05 19:28:51 -0500

    Add solution implementation for problem 2 and description file

---
+++ problem2/sol.py
@@ -0,0 +1,14 @@
+from utils import timer
+
+@timer
+def sol(upper_bound):
+    n0, n1, total = 1, 1, 0
+
+    while (n1 < upper_bound):
+        n0, n1 = n1, n0+n1
+        if (n1 % 2 == 0): total += n1
+    return total
+
+if __name__ == "__main__":
+    print(f'{sol(50)}')
+    print(f'{sol(4_000_000)}')
