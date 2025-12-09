b53a274918b8ca8542db70c905a5dea05a6b7f7b
ppcheng <p2cheng0331@gmail.com>
2025-12-09 15:51:36 -0500

    Problem 9: Special Pythagorean Triplet (#8)
    * Add initial implementation for Problem 9, including solution and description files
    
    * Refactor solution function to accept total as a parameter and update logic for finding Pythagorean triplet
    
    * Fix indentation in return statement for clarity in solution function
    
    * Fix variable assignment order in solution function for clarity
    
    * Remove unnecessary zero check in denominator condition for clarity in solution function
    
    * Remove redundant condition in triplet validation for clarity in solution function

---
+++ problem9/description.md
@@ -0,0 +1,9 @@
+# Problem 9: Special Pythagorean Triplet
+
+A Pythagorean triplet is a set of three natural numbers, 𝑎 <𝑏 <𝑐, for which,
+𝑎^2+𝑏^2=𝑐^2.
+
+For example, 3^2 +4^2 =9 +16 =25 =5^2.
+
+There exists exactly one Pythagorean triplet for which 𝑎 +𝑏 +𝑐 =1000.
+Find the product 𝑎⁢𝑏⁢𝑐.
---
+++ problem9/sol.py
@@ -0,0 +1,24 @@
+"""Solution for Problem 9."""
+
+from utils import timer
+
+
+@timer
+def sol(total):
+    # total = 1000
+    # b = 1000*(1000 - 2*a) / 2*(1000 - a)
+    # c = 1000 - b - a
+    # a < b < c and a + b + c = 1000 => a < 1000 / 3
+    for a in range(1, total // 3):
+        numerator = total * (total - 2 * a)
+        denominator = 2 * (total - a)
+        if numerator % denominator == 0:
+            b = numerator // denominator
+            c = total - b - a
+            if a < b:
+                return a * b * c
+    return None
+
+
+if __name__ == "__main__":
+    print(sol(1000))
